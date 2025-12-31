a42294b438ec39e750b49063d81729c0279b592b
shawn53a <shawn53a@gmail.com>
2025-12-31 14:01:26 -0600

    some extra notes

--- automatedRRA.sh
+++ automatedRRA.sh
@@ -1,3 +1,16 @@
 #Shawn Arreguin - 12/31/25
 #Read assembly, but more controlled
 
+# To run we need the following:
+#   ram
+#   memory
+#   SRRs
+#   cores
+#   reference proteome
+#   kraken DBs
+#   Semblans
+#   Home
+
+SRRs = $1
+
+
