bc7b54cd1abdca6bf25ad7b652fd89c5f211adad
shawn53a <shawn53a@gmail.com>
2025-12-31 15:55:55 -0600

    added getopts for single letter flags

--- automatedRRA.sh
+++ automatedRRA.sh
@@ -3,7 +3,6 @@
 
 # To run we need the following:
 #   ram
-#   memory
 #   SRRs
 #   cores
 #   reference proteome
@@ -11,6 +10,55 @@
 #   Semblans
 #   Home
 
-SRRs = $1
+usage() { echo -e "\n$0 \n\n[-r] amount of ram <number> \n[-t] number of threads <number> \n[-a] <tsv with SRR accesions> \n[-k] <path to Kraken DBs> \n[-s] <path to Semblans> \n[-p] <path to reference proteome> \n[-b] <path of working directory>" >&2; }
+
+while getopts ":hr:t:a:p:k:s:b:" OPTION; do
+        case "$OPTION" in
+                r)
+                        ram=${OPTARG}
+                        ;;
+                t)      
+                        threads=${OPTARG}
+                        ;;
+                a)
+                        SRR=${OPTARG}
+                        ;;
+
+                p)      refPro=${OPTARG}
+                        ;;
+
+                k)      KdbPATH=${OPTARG}
+                        ;;
+
+                s)      SPATH=${OPTARG}
+                        ;;
+
+                b)      HPATH=${OPTARG}
+                        ;;
+
+                :)      
+                        echo "Option -${OPTARG} requires an argument" >&2
+                        usage
+                        exit 1
+                        ;;
+                h)
+                        usage
+                        exit 0
+                        ;;
+                ?)
+                        echo "Invalid option: -${OPTARG}" >&2
+                        usage
+                        exit 1
+                        ;;
+        esac
+done    
+
+if [ -z "${ram}" ] || [ -z "${threads}" ] || [ -z "${SRR}" ] || [ -z "${refPro}" ] || [ -z "${KdbPATH}" ] || [ -z "${SPATH}" ] || [ -z "${HPATH}" ] ];then
+        usage
+        exit 1
+fi
+
+echo $ram
+echo $threads
 
 
